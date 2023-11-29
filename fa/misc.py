839e2498e4f2211edf364b041cf5f5281abd735d
nxy <nxy@qq.com>
2023-11-29 12:51:21 +0800

    modified:   fa/__pycache__/bspline.cpython-39.pyc 	modified:   fa/__pycache__/coilset.cpython-39.pyc 	modified:   fa/__pycache__/lossfunction.cpython-39.pyc 	modified:   fa/__pycache__/plot.cpython-39.pyc 	modified:   fa/bspline.py 	modified:   fa/coilset.py 	modified:   fa/lossfunction.py 	modified:   fa/main.py 	modified:   fa/plot.py 	modified:   fa/post.py 	modified:   init_args.py 	modified:   initfiles/init_args.json 	deleted:    initfiles/w7x/circle_coil_c.npy 	modified:   note.ipynb 	deleted:    results/circle/c_a0.npy 	deleted:    results/circle/c_a1.npy 	deleted:    results/circle/fr_a1.npy 	deleted:    results/circle/g_fc.npy 	deleted:    results/circle/gbc.npy 	deleted:    results/circle/glc.npy 	deleted:    results/circle/loss_a1.npy 	modified:   splprep/fppara.py 	modified:   test/ad_fd.py 	ellipse copy.focus 	ellipse.coils 	ellipse.focus 	initfiles/w7x/circle_coil_5.npy 	plasma.boundary 	results/circle/c_c1.npy 	results/circle/c_c2.npy 	results/circle/c_c3.npy 	results/circle/c_w7x_5.npy 	results/circle/fr_c1.npy 	results/circle/fr_c2.npy 	results/circle/fr_c3.npy 	results/circle/loss_c1.npy 	results/circle/loss_c2.npy 	results/circle/loss_c3.npy 	test/read_plasma.py 	w7x_std_30.coils

--- fa/misc.py
+++ fa/misc.py
@@ -20,17 +20,19 @@ with open('/home/nxy/codes/focusadd-spline/initfiles/init_args.json', 'r') as f:
 globals().update(args)
 
 
+# 测试bspline
 
-rc = np.load('/home/nxy/codes/focusadd-spline/initfiles/w7x/circle_coil_c.npy')
-coil = np.zeros((10, 71, 3))
-coil = coil.at[:, 3:-3, :].set(rc)
-coil = coil.at[:, -3:, :].set(rc[:, 1:4, :])
-coil = coil.at[:, :3, :].set(rc[:, -4:-1, :])
-np.save('/home/nxy/codes/focusadd-spline/initfiles/w7x/circle_coil_c.npy', coil)
+coil = np.load('/home/nxy/codes/focusadd-spline/initfiles/w7x/coil.npy')  
+# print(coil.shape)  # (50, 65, 3)
+c, bc = bspline.prep(coil, nc, ns+1, 3)
+# print(c0.shape)    # (50, 3, 67)
+rc = bspline.splev(bc, c[0])
+der1, wrk1 = bspline.der1_splev(bc, c[0])
+der2 = bspline.der2_splev(bc, wrk1)
+print(der2)
 
-
-
-
-
-
+# bn需要ns个点，第ns个点的导数与最后一个控制点有关吗
+# 仍然无关，基函数为0
+# 所以最后一个控制点只影响最后一段线段，且不包括该线段的起点（左开右闭）
+# 第一个控制点同样对应于第一段，但是包括起点（左闭右开）
 
